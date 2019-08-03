99225c6ecd6643bd5f84b8ec6022ef46a244d75a
xiaotaoge <1213544665@qq.com>
2019-08-03 20:03:11 +0800

    学生信息

--- src/main/java/com/zjwm/wyx/user/service/impl/HbbStudentServiceImpl.java
+++ src/main/java/com/zjwm/wyx/user/service/impl/HbbStudentServiceImpl.java
@@ -1,5 +1,5 @@
-/**
- * @Copyright: 2016-2019，中教网盟科技有限公司
+/*
+  @Copyright: 2016-2019，中教网盟科技有限公司
  * @FileName: HbbStudentServiceImpl
  * @Author: 王俊涛
  * @Date：2019/8/1 0001 8:52
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 /**
- * @Description:
+ * @Description: 学生信息操作
  * @version 2018.3
  *
  */
